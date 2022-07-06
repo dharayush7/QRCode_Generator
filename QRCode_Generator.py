a3c32b5877d22dc2bc44fa19341d89606571f411
AYUSH DHAR <108379129+dharayush7@users.noreply.github.com>
2022-07-06 13:04:53 +0530

    QRCode_Generator-graphic uploaded

--- QRCode_Generator.py
+++ QRCode_Generator.py
@@ -1,101 +1,326 @@
+from tkinter import *
+from tkinter import ttk
 import qrcode
-print(" ")
-print("             ***** QRCode_Generator *****")
-print(" ")
-url = input("Enter your url or sms:")
-print(" ")
-qrsz,bdsz =0,0
-colinp = ""
-a,b,c,d=0,0,0,0
-print("Select Size:")
-print("1. Small")
-print("2. Medium (Recomonded)")
-print("3. High")
-print(" ")
-print(" ")
-qrszinp = input ("Enter your Size no. :")
-if "1" in qrszinp:
-    qrsz = 10
-elif "2" in qrszinp:
-    qrsz = 20
-elif "3" in qrszinp:
-    qrsz = 40
-else :
-    a=1
-if a==0:
-    print(" ")
-    print("Select Border Size:")
-    print("1. Narrow")
-    print("2. Medium (Recomonded)")
-    print("3. High")
-    bdszinp = input(" Enter your Border Size: ")
-    if "1" in bdszinp:
-        bdsz = int(2)
-    elif "2" in bdszinp:
-        bdsz = int(5)
-    elif "3" in bdszinp:
-        bdsz = int(10)
-    else:
-        b = 1
-        
-else:
-    pass
 
-if a==0 and b==0:
-    print(" ")
-    print("Select QRCode Color:")
-    print("1. Black")
-    print("2. Red")
-    print("3. Blue")
-    print("4. Yellow")
-    print("5. Green")
-    print(" ")
-    colinp = input("Enter Color no. :")
-    if "1" in colinp :
-        col = "Black"
-    elif "2" in colinp:
-        col = "Red"
-    elif "3" in colinp:
-        col = "Blue"
-    elif "4" in colinp:
-        col = "Yellow"
-    elif "5" in colinp:
-        col = "Green"
-    else:
-        c = 1
 
-else:
-    pass
-
-if a==0 and b==0 and c==0:
-    print(" ")
-    name=input("Give a name of thise qrcode :")
-    if len(name)==0:
-        d = 1
-    else:
-        pass
+def gtdata():
+    global url
+    global nm
+    global size
+    global border
+    global colour
     
-else:
-    pass
+    url = urltxt.get()
+    nm = text1.get()
+    size = size_combo.get()
+    border = border_combo.get()
+    colour = colour_combo.get()
 
-if a==0 and b==0 and c==0 and d==0:
-
+def condata():
+    global consize
+    global conborder
+    global concolour
+    
+    if "Small" in size:
+        consize = 10
+    elif "Mediam(Recomonded)" in size:
+        consize = 20
+    elif "Big" in size:
+        consize = 40
+    else:
+        consize = 20
+    
+    if "Small" in border:
+        conborder = 2
+    elif "Mediam(Recomonded)" in border:
+        conborder = 5
+    elif "Big" in border:
+        conborder = 10
+    else:
+        conborder = 5
+    
+    concolour = colour.lower()
+        
+def main():
     qr = qrcode.QRCode(version=1,
                     error_correction = qrcode.constants.ERROR_CORRECT_H,
-                    box_size = qrsz,border = bdsz,)
+                    box_size = consize,border = conborder,)
     qr.add_data(url)
     qr.make(fit = True)
-    img = qr.make_image(fill_color= col)
-    img.save(name+"_qrcode.png")
-    print(" ")
-    print ("your QRCode save as " + name +"_qrcode.png")
-    print(" ")
-else :
-    pass
-if a==1 or b==1 or c==1:
-    print(" ")
-    print("invalid input!")
-    print(" ")
-else:
-    pass
+    img = qr.make_image(fill_color= concolour)
+    img.save(nm+"qrcode.png")
+    
 
+    
+def lb(text:"str",X_coordinate:"float",Y_coordinate:"float"):
+    label3 = Label(
+        canva,
+        text = text,
+        font = ("Roboto",20),
+        bg = "#ffffff",
+        highlightthickness=0,
+        fg="black",
+        bd=0,
+        anchor=W
+    )
+    label3.place(
+        x=X_coordinate,y=Y_coordinate,
+        width=500,
+        height=30
+    )
+
+
+def b0cl():
+    label2.config(text="Loading...")
+    try:
+        
+        lb("Getting Data...",0,35)
+        gtdata()
+        condata()
+        
+        lb("Processing..",0,70)
+        if nm == "" or url == "":
+            lb("A error occurred",0,105)
+            lb("URL or image name is empty!",0,140)
+        else:
+            main()
+            pt ="Your QR Code saved as "+ nm +"qrcode.png"
+            lb(pt,0,105)
+        
+    except:
+        lb("A error occurred",0,35)
+        
+    
+    
+window = Tk()
+window.title("QRCode_Generator")
+window.geometry("1000x700")
+window.config(bg="#9b9b9b")
+
+label = Label(
+    window, 
+    text = "QR Code Generetor",
+    font = ("Roboto",30,"bold"),
+    bg = "#9b9b9b",
+    fg="black",
+    bd=0,
+)
+label.place(
+    x=0,y=50,
+    width=1000,
+    height=40
+)
+
+label = Label(
+    window, 
+    text = "Enter Your URL or Massage bellow:",
+    font = ("Roboto",15),
+    bg = "#9b9b9b",
+    fg="black",
+    bd=0,
+    anchor=W
+)
+label.place(
+    x=50,y=125,
+    width=320,
+    height=25
+)
+urltxt = Entry(
+    window,
+    textvariable="",
+    font=("Roboto",20),
+    highlightthickness=1,
+    borderwidth=2,
+    # border=0,
+    bg="#ffffff"
+    
+)
+urltxt.place(
+    x= 50,y=150,
+    height=50,
+    width=900
+)
+
+size_list = [
+    "Small",
+    "Mediam(Recomonded)",
+    "Big"
+]
+
+label=Label(
+    window, 
+    text = "Size:",
+    font = ("Roboto",15),
+    bg = "#9b9b9b",
+    fg="black",
+    bd=0,
+)
+label.place(
+    x=50,y=220,
+    width=300,
+    height=20
+)
+
+size_combo = ttk.Combobox(
+    window,font=("Roboto",20),
+    value = size_list,
+)
+size_combo.place(
+    x=50,y=250,
+    width=293.3,
+    height=50
+)
+size_combo.set("Mediam(Recomonded)")
+
+border_list = [
+    "Small",
+    "Mediam(Recomonded)",
+    "Big"
+]
+
+label=Label(
+    window, 
+    text = "border:",
+    font = ("Roboto",15),
+    bg = "#9b9b9b",
+    fg="black",
+    bd=0,
+)
+label.place(
+    x=353.3,y=220,
+    width=293.3,
+    height=20
+)
+
+border_combo = ttk.Combobox(
+    window,font=("Roboto",20),
+    value = border_list,
+)
+border_combo.place(
+    x=353.3,y=250,
+    width=293.3,
+    height=50
+)
+border_combo.set("Mediam(Recomonded)")
+
+colour_list = [
+    "Black",
+    "Red",
+    "Blue",
+    "Yellow",
+    "Green"
+]
+
+label=Label(
+    window, 
+    text = "Colour:",
+    font = ("Roboto",15),
+    bg = "#9b9b9b",
+    fg="black",
+    bd=0,
+)
+label.place(
+    x=656.6,y=215,
+    width=293.3,
+    height=25
+)
+
+colour_combo = ttk.Combobox(
+    window,font=("Roboto",20),
+    value = colour_list,
+)
+colour_combo.place(
+    x=656.6,y=250,
+    width=293.3,
+    height=50
+)
+colour_combo.set("Black")
+
+label=Label(
+    window, 
+    text = "Enter image name",
+    font = ("Roboto",15),
+    bg = "#9b9b9b",
+    fg="black",
+    bd=0,
+)
+label.place(
+    x=50,y=315,
+    width=160,
+    height=25
+)
+
+text1 = Entry(
+    window,
+    textvariable = "",
+    font=("Roboto",20),
+    highlightthickness=1,
+    borderwidth=2,
+    # border=0,
+    bg="#ffffff"
+    
+)
+text1.place(
+    x= 50,y=340,
+    height=50,
+    width=450
+)
+
+b0 = Button(
+    window,
+    text="GENERET",
+    font=("Roboto",35),
+    relief=RAISED,
+    command=b0cl
+)
+b0.place(
+    x=600,y=340,
+    height=50,
+    width=280
+)
+
+label=Label(
+    window, 
+    text = "Status:",
+    font = ("Roboto",15),
+    bg = "#9b9b9b",
+    fg="#262626",
+    bd=0,
+)
+label.place(
+    x=50,y=420,
+    width=450,
+    height=25
+)
+
+canva=Canvas(
+    window,
+    background="#ffffff",
+    bd=1,
+    
+)
+canva.place(
+    x=50,y=450,
+    height=200,
+    width=500
+)
+
+label2=Label(
+    canva, 
+    text = "empty",
+    font = ("Roboto",20),
+    bg = "#ffffff",
+    highlightthickness=0,
+    fg="black",
+    bd=0,
+    anchor=W
+)
+label2.place(
+    x=0,y=0,
+    width=450,
+    height=30
+)
+
+
+
+window.mainloop()
--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 
-# QRCode_Generator
+# QRCode_Generator-graphic
 
-Only run in the terminal. For graphix Swich branch to graphix branch.
-
-
+It is graphix version.
 ## Description :
 
-It is a QR Code Generator. Run the file in cmd (windows) or teminal (Apple or Linux).  Then, enter a massage or url. Select size. There are 3 size options [Small, Medium (Recomonded), High]. After, select border size. There are 3 border size options [Small, Medium (Recomonded), High]. Next, select QRCode color. There are 5 color options [Black, Red, Blue, Yellow, Green]. At last, Give a name of QRCode. Filnally your QRCode save as "name_qrcode.png" in this directory.
-
+It is a QR Code Generator. Run the file in cmd (windows) or teminal (Apple or Linux).  Then, enter a massage or url. Select size. There are 3 size options [Small, Medium (Recomonded), High]. After, select border size. There are 3 border size options [Small, Medium (Recomonded), High]. Next, select QRCode color. There are 5 color options [Black, Red, Blue, Yellow, Green]. At last, Give a name of QRCode and click to "GENERET". Filnally your QRCode save as "name_qrcode.png" in this directory. Every thing is display in status column.
 
 ##  Requiredment :
 
+- from tkinter import *
+- from tkinter import ttk
 - import qrcode
 
 To install this module run this command in terminal :
+
+```bash
+  sudo apt-get install python3-tk 
+```
+And then,
 
 ```bash
   pip install qrcode
@@ -25,7 +29,7 @@ At frist clone thouse file or download a zip. Extract this zip file.
 And run those command. 
 
 ```bash
-  cd /downloads/QRCode_Generator-main
+  cd /downloads/QRCode_Generator-graphic
 ```
 And then, 
 
@@ -37,15 +41,15 @@ And then,
 
 ## Screenshots
 
-![App Screenshot](https://github.com/dharayush7/pyimage/blob/main/QRCode_Generator/image1.png?raw=true)
+![App Screenshot](https://github.com/dharayush7/pyimage/blob/main/QRCode_Generator-graphic/image1.png?raw=true)
 
-![App Screenshot](https://github.com/dharayush7/pyimage/blob/main/QRCode_Generator/image2.png?raw=true)
+![App Screenshot](https://github.com/dharayush7/pyimage/blob/main/QRCode_Generator-graphic/image2.png?raw=true)
 
-![App Screenshot](https://github.com/dharayush7/pyimage/blob/main/QRCode_Generator/image3.png?raw=true)
+![App Screenshot](https://github.com/dharayush7/pyimage/blob/main/QRCode_Generator-graphic/image3.png?raw=true)
 
-![App Screenshot](https://github.com/dharayush7/pyimage/blob/main/QRCode_Generator/image4.png?raw=true)
+![App Screenshot](https://github.com/dharayush7/pyimage/blob/main/QRCode_Generator-graphic/image4.png?raw=true)
 
-![App Screenshot](https://github.com/dharayush7/pyimage/blob/main/QRCode_Generator/image5.png?raw=true)
+
 
 
 
